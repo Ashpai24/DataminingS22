5d9e8960683fd7e8982ef16877ce490a8871ad89
Ashpai24 <apai2@wpi.edu>
2022-02-22 21:42:54 -0500

    Printing to File Works -- Last thing to complete is Report

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import re
 import nltk
 
-nltk.help.upenn_tagset()
 # Constructing Pattern for words with only capital letters
 pattern = re.compile(r"('?[A-Z],?'?\s?[a-z][^\d.!?]*-?[-\".!?])")
 
@@ -14,36 +13,46 @@ with open('hamlet_act1.txt', 'r') as f:
 sentences = pattern.findall(contents)
 print("Number of Sentences Mined: ", len(sentences))
 
+# Pattern for Getting Noun and Verb Pairs
 pattern1 = re.compile(r"\('[A-Z]?[a-z]+',\s'NN[A-Z]?[A-Z]?'\),\s\('[A-Z]?[a-z]+',\s'VB[A-Z]?'\)")
+
+# Pattern for Getting Pronoun Verb Pairs
 pattern2 = re.compile(r"\('[A-Z]?[a-z]+',\s'PRP'\),\s\('[A-Z]?[a-z]+',\s'VB[A-Z]?'\)")
 
 noun_verb_pairs = []
 pronoun_verb_pairs = []
+
+# Tokenizing each Sentence to see POS (Part of Speech)
 for word in sentences:
     sentence_with_tagset = nltk.pos_tag(nltk.word_tokenize(word))
-    print(str(sentence_with_tagset))
     matches_n_v = pattern1.findall(str(sentence_with_tagset))
     matches_pn_v = pattern2.findall(str(sentence_with_tagset))
+
+    # Adding each Regex Match to A list
     for match1 in matches_n_v:
         noun_verb_pairs.append(match1.lower())
     for match2 in matches_pn_v:
         pronoun_verb_pairs.append(match2.lower())
 
+# Printing the Len of each Calculated List
 print(len(noun_verb_pairs))
 print(len(pronoun_verb_pairs))
 
 combined_list = noun_verb_pairs + pronoun_verb_pairs
-print(combined_list)
+
+# '(w, POS), (w2, POS)' Structure --> (w1, w2, w3, ... wN)
 pattern_parse = re.compile(r"([a-z]+)(?=',)")
 wordlist = []
-
 for element in combined_list:
     word_pair_matches = pattern_parse.findall(str(element))
     for match in word_pair_matches:
         wordlist.append(match)
 
+# Converting (w1, w2, w3, ... wN) --> [(w1, w2), (w3, w4), ... (wN-1, wN)]
 paired_list = [wordlist[i] + ' ' + wordlist[i + 1] for i in range(0, len(wordlist) - 1, 2)]
+print(paired_list)
 
+# Using Dictionary to Count Number of Pair Occurrences
 dictionary = {}
 for item in paired_list:
     if item in dictionary:
@@ -51,5 +60,13 @@ for item in paired_list:
     else:
         dictionary[item] = 1
 
-'''for k, v in dictionary.items():
-    print(k, v)'''
+# Printing top 10 K, V Pairs of Dictionary
+f = open("Highest_Occuring_Bigrams.txt", "a")
+i = 0
+sorted_keys = sorted(dictionary, key=dictionary.get, reverse=True)
+for r in sorted_keys:
+    if i < 10:
+        f.write("Word Pair: " + r + " --> Number Occurrences: " + str(dictionary[r]) + "\n")
+    i += 1
+
+f.close()
